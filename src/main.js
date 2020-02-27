10e61d8064729800052264d008ba781f4976cdb1
Paulo Cesar <email@pocesar.e4ward.com>
2020-02-27 17:07:28 -0300

    use a named store

--- src/main.js
+++ src/main.js
@@ -4,13 +4,23 @@ const { initialize } = require('./functions');
 const { log } = Apify.utils;
 
 Apify.main(async () => {
+    const {
+        storageName = 'user-agents',
+    } = await Apify.getInput();
+
     const userAgents = await initialize();
 
     log.info(`Executing at ${new Date().toISOString()}`);
 
     const total = Object.entries(userAgents).reduce((o, [_, s]) => o + s.length, 0);
 
-    await Apify.setValue('OUTPUT', {
+    if (total < 1000) {
+        throw new Error(`Current total count ${total} looks like it's missing data`);
+    }
+
+    const kv = await Apify.openKeyValueStore(storageName);
+
+    await kv.setValue('LIST', {
         updated: new Date().toISOString(),
         totalCount: total,
         ...userAgents,
